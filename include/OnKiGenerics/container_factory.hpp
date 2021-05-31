5ed5ca82449f38a5facf730e027bfe1c49c66ddf
Whobuilder <onurkimilli@gmail.com>
2021-05-31 12:08:38 +0200

    Add initial value as function parameter
    Add template keyword to the template dependent template functions
    It doesnt compile on linux otherwise.

--- include/OnKiGenerics/container_factory.hpp
+++ include/OnKiGenerics/container_factory.hpp
@@ -3,54 +3,55 @@
 #include <cstddef>
 #include <array>
 #include <type_traits>
+#include <OnKiGenerics/create_array.hpp>
 namespace onkigenerics {
 
 template<typename Container>
 struct ContainerFactory
 {
-    using value_type = typename Container::value_type;
+    using value_type = std::remove_pointer_t<std::remove_reference_t<std::remove_cv_t<typename Container::value_type>>>;
 
-    static Container create(std::size_t size)
+    static Container create(std::size_t size, value_type initial_value = value_type{})
     {
-        return Container(size);
+        return Container(size, initial_value);
     }
 
     template<std::ptrdiff_t N, typename BaseContainer>
-    static Container create_with_incremented_size(BaseContainer &&base)
+    static Container create_with_incremented_size(BaseContainer &&base, value_type initial_value = value_type{})
     {
-        return Container(static_cast<std::size_t>(static_cast<std::ptrdiff_t>(base.size()) + N));
+        return Container(static_cast<std::size_t>(static_cast<std::ptrdiff_t>(base.size()) + N), initial_value);
     }
 };
 template<typename T, std::size_t N>
 struct ContainerFactory<std::array<T, N>>
 {
-    using value_type = T;
+    using value_type = std::remove_pointer_t<std::remove_reference_t<std::remove_cv_t<T>>>;
 
-    static std::array<T, N> create(std::size_t)
+    static std::array<T, N> create(std::size_t, value_type initial_value = value_type{})
     {
-        return {};
+        return create_array<value_type, N>(initial_value);
     }
     template<std::ptrdiff_t Difference, typename BaseContainer>
-    static std::array<T, static_cast<std::size_t>(static_cast<std::ptrdiff_t>(N) + Difference)> create_with_incremented_size(BaseContainer &&)
+    static std::array<T, static_cast<std::size_t>(static_cast<std::ptrdiff_t>(N) + Difference)> create_with_incremented_size(BaseContainer &&, value_type initial_value = value_type{})
     {
-        return {};
+        return create_array<value_type, static_cast<std::size_t>(static_cast<std::ptrdiff_t>(N) + Difference)>(initial_value);
     }
 };
 
 template<typename T, std::size_t N>
 struct ContainerFactory<T[N]>
 {
-    using value_type = T;
+    using value_type = std::remove_pointer_t<std::remove_reference_t<std::remove_cv_t<T>>>;
 
-    static std::array<T, N> create(std::size_t)
+    static std::array<value_type, N> create(std::size_t, value_type initial_value = value_type{})
     {
-        return {};
+        return create_array<value_type, N>(initial_value);
     }
 
     template<std::ptrdiff_t Difference, typename BaseContainer>
-    static std::array<T, static_cast<std::size_t>(static_cast<std::ptrdiff_t>(N) + Difference)> create_with_incremented_size(BaseContainer &&)
+    static std::array<T, static_cast<std::size_t>(static_cast<std::ptrdiff_t>(N) + Difference)> create_with_incremented_size(BaseContainer &&, value_type initial_value = value_type{})
     {
-        return {};
+        return create_array<value_type, static_cast<std::size_t>(static_cast<std::ptrdiff_t>(N) + Difference)>(initial_value);
     }
 };
 
